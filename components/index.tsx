352577d377cc5ff662fc84724d98943ce41538d0
talharamay49 <talharamay49@gmail.com>
2023-06-10 13:33:31 +0500

    create index for glov=bal exports

---
+++ components/index.tsx
@@ -0,0 +1,10 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+export {
+    Navbar,
+    Hero,
+    CustomButton,
+    Footer
+}
