830cbd1abf3b5e044d0547a2f7ebc6d24f08a0bb
talharamay49 <talharamay49@gmail.com>
2023-06-10 16:24:33 +0500

    create full page

--- components/index.tsx
+++ components/index.tsx
@@ -2,9 +2,17 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
 export {
     Navbar,
     Hero,
     CustomButton,
-    Footer
+    Footer,
+    SearchBar,
+    CustomFilter,
+    CarCard,
+    ShowMore
 }
